03022941844e2ef1ae27f34e598758b4e70655b2
chen lei <164287643@qq.com>
2018-08-12 10:25:07 +0800

    123

--- 1-server.js
+++ 1-server.js
@@ -1,14 +1,35 @@
-var http = require('http');
-http.createServer(function (request, response) {
+// var http = require('http');
+// http.createServer(function (request, response) {
 
-    // 发送 HTTP 头部 
-    // HTTP 状态值: 200 : OK
-    // 内容类型: text/plain
-    response.writeHead(200, {'Content-Type': 'text/plain'});
+//     // 发送 HTTP 头部 
+//     // HTTP 状态值: 200 : OK
+//     // 内容类型: text/plain
+//     response.writeHead(200, {'Content-Type': 'text/plain'});
 
-    // 发送响应数据 "Hello World"
-    response.end('Hello World\n');
-}).listen(8888);
+//     // 发送响应数据 "Hello World"
+//     response.end('Hello World\n');
+// }).listen(8888);
 
-// 终端打印如下信息
-console.log('Server running at http://127.0.0.1:8888/');
+// // 终端打印如下信息
+// console.log('Server running at http://127.0.0.1:8888/');
+
+var express = require('express');
+var app = express();
+
+//中间件
+app.use(function (req, res, next) {
+    console.log('Time:', Date.now());
+    next();
+});
+
+
+app.get('/', function (req, res) {
+    res.send('Hello World! This is express server');
+});
+
+var server = app.listen(3000, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('Example app listening at http://%s:%s', host, port);
+});
